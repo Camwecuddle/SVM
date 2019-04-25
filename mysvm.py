330fca052632abb577979f0a71e18ce79c753e60
camronvick <camronvick@aol.com>
2019-04-25 15:53:49 -0500

    Altered code to work with any data set hopefully. Iris Data set test gets 100% correct classification

--- mysvm.py
+++ mysvm.py
@@ -16,11 +16,11 @@ def train(data, targets, k, C=None):
     K = np.array([[k(x_1, x_2) for x_2 in data] for x_1 in data])
     # Compute P
     P = targets * targets.transpose() * K
-    q = -np.ones((6, 1))
-    G = -np.eye(6)
-    h = np.zeros((6, 1))
-    A = targets.reshape(1, 6)
-    print(A)
+    q = -np.ones((len(data), 1))
+    G = -np.eye(len(data))
+    h = np.zeros((len(data), 1))
+    A = targets.reshape(1, len(data))
+    A = A.astype(float)
 
     # We'll turn this into a vector in the following line when we pass
     # all these to qp
@@ -32,21 +32,15 @@ def train(data, targets, k, C=None):
 
     # Retreive the lagrange multipliers
     lagrange_multipliers = np.array(sol['x'])
-    print("Lagrange Multipliers")
-    print(lagrange_multipliers)
 
     # Identify (the indices of) the support vectors
     threshold = 1e-5
     support_vectors = np.where(lagrange_multipliers > threshold)[0]
-    print("Support Vectors")
-    print(support_vectors)
 
     # Compute the intercept
-    w_0 = sum([targets[j][0] - sum([lagrange_multipliers[i] * targets[i][0] * k(data[i], data[j])
+    w_0 = sum([targets[j] - sum([lagrange_multipliers[i] * targets[i] * k(data[i], data[j])
         for i in support_vectors])
            for j in support_vectors]) / len(support_vectors)
-    # print("w_0")
-    # print(w_0)
 
     complete_svm = SVM(lagrange_multipliers, targets, k, data, support_vectors, w_0)
     return complete_svm
@@ -54,15 +48,10 @@ def train(data, targets, k, C=None):
 
 # Binary classification
 def classify(svm, inputs):
-    # print("l_multipliers")
-    # print(svm.l_multipliers)
-    # print("targets[i]")
-    # print(svm.targets)
     classifications = [classifyOne(svm, x) for x in inputs]
     return classifications
 
 # helper function for classify
 def classifyOne(svm, input):
-    return np.sign(sum([svm.l_multipliers[i] * svm.targets[i][0] * svm.k(svm.data[i], input)
-                        for i in svm.sup_vectors]) + svm.w_0)
+    return np.sign(sum([svm.l_multipliers[i] * svm.targets[i] * svm.k(svm.data[i], input) for i in svm.sup_vectors]) + svm.w_0)
 
--- test_svm.py
+++ test_svm.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics.pairwise import rbf_kernel as sklrbf
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import mysvm
@@ -7,31 +8,48 @@ import mysvm
 def linear(x1, x2):
     return 1 + np.dot(x1, x2)
 
+def rbf(x1, x2) :
+    return sklrbf([x1], [x2])[0][0]
+
 data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33, random_state=42)
+
+for i in range(len(y_train)):
+    if(y_train[i] > 0):
+        y_train[i] = 1.0
+    else:
+        y_train[i] = -1.0
+    print(y_train[i])
+
+for i in range(len(y_test)):
+    if(y_test[i] > 0):
+        y_test[i] = 1.0
+    else:
+        y_test[i] = -1.0
+    print(y_test[i])
 
 
 
 # Six points on the x axis
-X = np.array([[1.0, 0.0], [2.0, 0.0], [3.0, 0.0],
-              [-1.0, 0.0], [-2.0, 0.0], [-3.0, 0.0]])
+X = np.array([[1.0, 0.0], [2.0, 0.0], [3.0, 0.0], [4.0, 0.0],
+              [-1.0, 0.0], [-2.0, 0.0], [-3.0, 0.0], [-4.0, 0.0]])
 
 # Points on the positive x axis are in one class, negative in the other
-Y = np.array([[1.0], [1.0], [1.0], [-1.0], [-1.0], [-1.0]])
+Y = np.array([[1.0], [1.0], [1.0], [1.0], [-1.0], [-1.0], [-1.0], [-1.0]])
 
 # Some other points, as a test set
-X_test = [[4.0, 5.0], [1.5, 0.0], [7.5, 10.0],
-          [-5.0, 0.0], [-2.0, -4.0], [-2.0, 4.0]]
+# X_test = [[4.0, 5.0], [1.5, 0.0], [7.5, 10.0],
+#           [-5.0, 0.0], [-2.0, -4.0], [-2.0, 4.0]]
 
-y_test = [1.0, 1.0, 1.0, -1.0, -1.0, -1.0]
+# y_test = [1.0, 1.0, 1.0, -1.0, -1.0, -1.0]
 
 
 def accuracy(trained_svm, X_test, y_test):
     score = 0
     classified = mysvm.classify(trained_svm, X_test)
     for i in range(len(classified)):
-        # print(classified[i][0])
-        # print(y_test[i])
+        print(str(y_test[i]) + ": classified as :" + str(classified[i][0]))
+
         if(classified[i] == y_test[i]):
             score += 1
     print("Score: " + str(score) + "/" + str(len(y_test)) + "  " + str((score/len(y_test)) * 100) + "%")
@@ -48,9 +66,9 @@ def accuracy(trained_svm, X_test, y_test):
 # mysvm.classify(mysvm.train(X_train, y_train, linear), X_test)
 
 
-trained_svm = mysvm.train(X, Y, linear)
-accuracy(trained_svm, X_test, y_test)
-
-# trained_svm = mysvm.train(X_train, y_train, linear)
+# trained_svm = mysvm.train(X, Y, linear)
 # accuracy(trained_svm, X_test, y_test)
+
+trained_svm = mysvm.train(X_train, y_train, rbf)
+accuracy(trained_svm, X_test, y_test)
 
